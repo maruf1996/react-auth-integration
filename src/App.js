dcd86271216a9ddbe1ba865882a9d7b67a51e85a
maruf1996 <maruf3813@gmail.com>
2021-10-17 20:54:09 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './component/Header/Header';
+import Home from './component/Home/Home';
+import Register from './component/Register/Register';
+import Login from './component/Login/Login';
+import AuthProvider from './context/AuthProvider';
+import Shipping from './component/Shipping/Shipping';
+import PrivateRoute from './component/PrivateRoute/PrivateRoute';
+import PlaceOrder from './component/PlaceOrder/PlaceOrder';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <AuthProvider>
+        <BrowserRouter>
+            <Header></Header>
+            <Switch>
+              <Route exact path="/">
+                <Home></Home>
+              </Route>
+              <Route path="/home">
+                <Home></Home>
+              </Route>
+              <Route path="/login">
+                <Login></Login>
+              </Route>
+              <Route path="/register">
+                <Register></Register>
+              </Route>
+              <PrivateRoute path="/shipping">
+                <Shipping></Shipping>
+              </PrivateRoute>
+              <PrivateRoute path="/placeorder">
+                <PlaceOrder></PlaceOrder>
+              </PrivateRoute>
+            </Switch>
+          </BrowserRouter>
+       </AuthProvider>
     </div>
   );
 }
